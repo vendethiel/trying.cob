1774f716c29026f417abdc5da7dd9008a7ff6683
vendethiel <vendethiel@hotmail.fr>
2015-05-26 13:03:09 +0200

    After much search, I found `perform forever`
    read-until-we-die reads until the user stops inputting numbers

---
+++ read-until-we-die.cob
@@ -0,0 +1,19 @@
+       identification division.
+       program-id. read-until-we-die.
+
+       data division.
+       working-storage section.
+       01 user-input pic 9(10).
+       01 total pic 9(10).
+
+       procedure division.
+           perform forever
+             display "Enter a number, or empty to stop"
+             accept user-input
+             if user-input = 0
+               exit perform
+             end-if
+             add user-input to total
+           end-perform
+           display "The sum is " total
+           .
