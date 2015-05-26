4603bdb7e123b5b4fffc5eaccc0b5ecef47bbaca
vendethiel <vendethiel@hotmail.fr>
2015-05-26 12:51:26 +0200

    read N integers, better

--- read-n-integers.cob
+++ read-n-integers.cob
@@ -1,9 +1,10 @@
+       identification division.
        program-id. read-n-integers.
 
        data division.
        working-storage section.
-       01 i pic 9(1).
-       01 cnt pic 9(10).
+       01 i pic 9(3).
+       01 cnt pic 9(3).
        01 user-input pic 9(10).
        01 total pic 9(10).
 
