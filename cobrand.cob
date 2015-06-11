c1596cd201e1e7e2cce51dfe304daf365cfea324
vendethiel <vendethiel@hotmail.fr>
2015-06-11 12:50:48 +0200

    Use the "repository" thing
    I have writing "function" all over the place.

--- cobrand.cob
+++ cobrand.cob
@@ -1,6 +1,11 @@
        identification division.
        program-id. cobrand.
 
+       environment division.
+       configuration section.
+       repository.
+           function all intrinsic.
+
        data division.
        linkage section.
            copy "cobrand-data.copy".
@@ -8,12 +13,12 @@
        procedure division using init-status, randed, cur-date.
            if init-status = "N" then
              set did-init to true
-             move function current-date to cur-date
-             compute randed = function random (current-minute * 1000 +
+             move current-date to cur-date
+             compute randed = random (current-minute * 1000 +
               current-second)
            else
              display ":v"
-             compute randed = function random ()
+             compute randed = random ()
            end-if
            goback
            .
